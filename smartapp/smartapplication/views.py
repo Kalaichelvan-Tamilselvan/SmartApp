b6ccd22b998eaa1b564b960b69e4cca0b3e8b717
Kalaichelvan Tamil Selvan <karthikalaichelvan@gmail.com>
2022-11-19 00:11:36 +0100

    Completed logic, views, index.
    Added all the logic, views and index page with results displayed on the guide page.

--- smartapp/smartapplication/views.py
+++ smartapp/smartapplication/views.py
@@ -3,7 +3,16 @@ from django.urls import reverse_lazy
 from django.views.generic import TemplateView, FormView
 
 from .forms import TestForm
-from .logic import punctuation_removal, text_upper
+from .logic import (punctuation_removal,
+                    text_upper,
+                    text_lower,
+                    remove_newline,
+                    extra_space_remove,
+                    count_characters,
+                    text_spellchecker,
+                    summary_generate,
+                    remove_stopwords,
+                    )
 
 
 # Create your views here.
@@ -35,9 +44,39 @@ def GuidePage(request):
     if bool(form_data['remove_punctuations']):
         changed_data['Remove Punctuations:'] = punctuation_removal(text)
         current_text = punctuation_removal(current_text)
+
     if bool(form_data['upper_case']):
         changed_data['Upper Case:'] = text_upper(current_text)
         current_text = text_upper(current_text)
+
+    if bool(form_data['lower_case']):
+        changed_data['Lower Case:'] = text_lower(current_text)
+        current_text = text_lower(current_text)
+
+    if bool(form_data['new_line_remove']):
+        changed_data['New Line Remove:'] = remove_newline(current_text)
+        current_text = remove_newline(current_text)
+
+    if bool(form_data['extra_space_remove']):
+        changed_data['Extra Space Remove:'] = extra_space_remove(current_text)
+        current_text = extra_space_remove(current_text)
+
+    if bool(form_data['count_characters']):
+        changed_data['Count Characters:'] = count_characters(current_text)
+        current_text = count_characters(current_text)
+
+    if bool(form_data['spell_check']):
+        changed_data['Spell Check:'] = text_spellchecker(current_text)
+        current_text = text_spellchecker(current_text)
+
+    if bool(form_data['generate_word_summary']):
+        changed_data['Generate summary:'] = summary_generate(current_text)
+        current_text = summary_generate(current_text)
+
+    if bool(form_data['remove_stop_words']):
+        changed_data['Remove Stop Word:'] = remove_stopwords(current_text)
+        current_text = remove_stopwords(current_text)
+
     context = {
         'current_text': current_text,
         'content': changed_data
